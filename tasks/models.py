53f62f0a15ade88d3bbf3cc413ee8c6643abfff7
Mariia <tiurina.maria@gmail.com>
2018-08-23 16:31:52 +0200

    My Django Girls project, first commit

---
+++ tasks/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    level = models.PositiveSmallIntegerField()
+    topic = models.CharField(max_length=100)
+    content = models.TextField()
+    correct_answer = models.CharField(max_length=50)
+
+class Answer(models.Model):
+    content = models.TextField()
+    name = models.ForeignKey(
+        'Task',
+        on_delete=models.CASCADE,
+    )
